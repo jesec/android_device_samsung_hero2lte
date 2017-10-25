160006b51658af1604b05e160c8d79bce23c4904
Jesse Chan <jc@lineageos.org>
2017-10-24 21:55:06 -0700

    hero2lte: initial commit
    Change-Id: I4e2452848a4e1a99ca69984d968dcd8c0138587f

---
+++ BoardConfig.mk
@@ -0,0 +1,9 @@
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/samsung/universal8890
+TARGET_KERNEL_CONFIG := exynos8890-hero2lte_defconfig
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := hero2lteskt,hero2ltektt,hero2ltelgt,hero2ltekor,hero2ltebmc,hero2ltexx,hero2lte
+
+# Inherit common board flags
+include device/samsung/hero-common/BoardConfigCommon.mk
